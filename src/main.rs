958d4eb93cc46ab4e62e6147c560514990e65223
Zefanja Jobse <zefanjaj@gmail.com>
2022-11-08 07:58:37 +0100

    use failedlist instead

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,8 @@ async fn main() -> anyhow::Result<()> {
             Err(e) => log::error!("Failed to send new manager info {:#?}", e),
         };
 
-        let mut game_results: Vec<Option<results::RegionResult>> = vec![];
+        let mut game_results: HashMap<String, results::RegionResult> = HashMap::new();
+        let mut failed_games: Vec<&str> = vec![];
 
         let old_games =  HashMap::from([
             ("bf2-playbf2", "playbf2"),
@@ -52,13 +53,15 @@ async fn main() -> anyhow::Result<()> {
             ("bfvietnam-openspy", "openspy")
         ]);
         for (key, value) in old_games.into_iter() {
-            game_results.push(match old_games::push_old_games(&influx_client, &mut mongo_client, key, value).await {
-                Ok(game_result) => Some(game_result),
+            match old_games::push_old_games(&influx_client, &mut mongo_client, key, value).await {
+                Ok(game_result) => {
+                    game_results.insert(key.to_string(), game_result)
+                },
                 Err(e) => {
                     log::error!("Failed oldgame: {}, with reason: {:#?}", key, e);
-                    None
+                    failed_games.push(key);
                 },
-            });
+            };
         }
 
         let sparta_games = HashMap::from([
@@ -67,16 +70,16 @@ async fn main() -> anyhow::Result<()> {
             ("bf4", "bf4")
         ]);
         for (key, value) in sparta_games.into_iter() {
-            game_results.push(match companion::gather_companion(&influx_client, sessions.get(key).unwrap_or(&empty_game_hash).to_owned(), cookie.clone(), key, value).await {
+            match companion::gather_companion(&influx_client, sessions.get(key).unwrap_or(&empty_game_hash).to_owned(), cookie.clone(), key, value).await {
                 Ok((session, platform_result)) => {
                     sessions.insert(key.to_string(), session);
-                    Some(platform_result)
+                    game_results.insert(key.to_string(), platform_result);
                 },
                 Err(e) => {
                     log::error!("Failed sparta_game: {}, with reason: {:#?}", key, e);
-                    None
+                    failed_games.push(key);
                 },
-            });
+            };
         }
         // pc only!
         let battlelog_games = HashMap::from([
@@ -85,32 +88,32 @@ async fn main() -> anyhow::Result<()> {
             ("bfh", "https://battlelog.battlefield.com/bfh/servers/getServers/pc/")
         ]);
         for (key, value) in battlelog_games {
-            game_results.push(match battlelog::gather_battlelog(&influx_client, key, value).await {
-                Ok(game_result) => Some(game_result),
+            match battlelog::gather_battlelog(&influx_client, key, value).await {
+                Ok(game_result) => {
+                    game_results.insert(key.to_string(), game_result);
+                },
                 Err(e) => {
                     log::error!("Failed battlelog_game: {}, with reason: {:#?}", key, e);
-                    None
+                    failed_games.push(key);
                 },
-            });
+            };
         }
-        game_results.push(match battlefield_grpc::gather_grpc(&influx_client, sessions.get("kingston").unwrap_or(&empty_game_hash).to_owned(), cookie.clone()).await {
+        match battlefield_grpc::gather_grpc(&influx_client, sessions.get("kingston").unwrap_or(&empty_game_hash).to_owned(), cookie.clone()).await {
             Ok((session, game_result)) => {
                 sessions.insert("kingston".to_string(), session);
-                Some(game_result)
+                game_results.insert("kingston".to_string(), game_result);
             },
             Err(e) => {
                 log::error!("Failed kingston_grpc, with reason: {:#?}", e);
-                None
+                failed_games.push("kingston");
             },
-        });
+        };
         
         // if no games failed, make global array
-        if game_results.iter().any(|result| !result.is_none()) {
-            let result: HashMap<String, results::RegionResult> = HashMap::new();
-            for (index, game) in game_results.iter().enumerate() {
-                result.insert(index.to_string(), game.unwrap());
-            }
-            let global_result = results::combine_region_players("global", &result).await;
+        if failed_games.iter().any(|&value| vec!["bf3", "bf4", "bfh", "tunguska", "casablanca", "kingston"].contains(&value)) {
+            log::error!("1 of the important games failed to gather, skipping global array...");
+        } else {
+            let global_result = results::combine_region_players("global", &game_results).await;
             match influx_db::push_totals(&influx_client, global_result).await {
                 Ok(_) => {},
                 Err(e) => log::error!("Failed to push global games array: {:#?}", e),
